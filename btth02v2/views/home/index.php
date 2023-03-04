34b41b9bb6a11f5b8fad3c44e71358cca9a134ed
Chi Cong <chicong422002@gmail.com>
2023-03-04 23:04:41 +0700

    add bootstrap and font-awesome

--- btth02v2/views/home/index.php
+++ btth02v2/views/home/index.php
@@ -1,21 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./home.css">
+    <script src="/CSE485_2023_BTTH02/btth02v2/assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="/CSE485_2023_BTTH02/btth02v2/assets/font-awesome/brands.min.js"></script>
+    <script src="/CSE485_2023_BTTH02/btth02v2/assets/font-awesome/fontawesome.min.js"></script>
+    <script src="/CSE485_2023_BTTH02/btth02v2/assets/font-awesome/solid.min.js"></script>
     <title>Document</title>
 </head>
+
 <body>
     <ul>
         <li><a href="./index.php">Trang chủ</a></li>
         <li><a href="./index.php?controller=article&action=list">Bài viết</a></li>
     </ul>
-    <h1>Tôi là TRANG CHỦ</h1>
-    <?php
-        foreach($articles as $article){
-            echo "<p>{$article->getTitle()}</p>";
-        }
-    ?>
+
 </body>
+
 </html>
